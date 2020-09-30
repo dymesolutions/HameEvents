ae9f60b57e0040738b5f8684a10b1535e4a1161f
Kristian Wrang <kristian.wrang@dymesolutions.com>
2020-09-30 15:25:07 +0300

    [ci] GDAL fix attempt

--- install_gdal_ubuntu.sh
+++ install_gdal_ubuntu.sh
@@ -3,8 +3,8 @@ set -e
 
 add-apt-repository ppa:ubuntugis/ppa
 apt-get update
+apt-get -y install python3.7-dev
 apt-get -y install gdal-bin
 apt-get -y install libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-pip install GDAL
