76e394b0f6f72b0cb8488c19bd6f7b02fc301826
Kristian Wrang <kristian.wrang@dymesolutions.com>
2020-09-30 14:58:37 +0300

    [ci] Auto approve installs

--- install_gdal_ubuntu.sh
+++ install_gdal_ubuntu.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
-add-apt-repository ppa:ubuntugis/ppa && apt-get update
+add-apt-repository ppa:ubuntugis/ppa
 apt-get update
-apt-get install gdal-bin
-apt-get install libgdal-dev
+apt-get -y install gdal-bin
+apt-get -y install libgdal-dev
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
 pip install GDAL
